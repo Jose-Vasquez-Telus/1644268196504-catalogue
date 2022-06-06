c3f5ed0baa81feb0d62287c777861e6b12f7eb92
Jose-Vasquez-Telus <104869687+Jose-Vasquez-Telus@users.noreply.github.com>
2022-06-06 08:14:17 -0600

    Create index page

--- src/ui/components/Home/index.js
+++ src/ui/components/Home/index.js
@@ -1,6 +1,28 @@
-import React from 'react';
-import Heading from '@tds/core-heading';
-
-const Home = () => <Heading level="h1">Device Catalogue</Heading>;
+import React from "react";
+import Heading from "@tds/core-heading";
+import FlexGrid from "@tds/core-flex-grid";
+import ChevronLink from "@tds/core-chevron-link";
+import Box from "@tds/core-box";
+const Home = () => (
+  <Box below={8}>
+    <FlexGrid>
+      <FlexGrid.Row>
+        <FlexGrid.Col>
+          <Box style={{ background: "#4b286d" }} inset={4} between={3}>
+            <Heading invert level="h1">
+              Devices
+            </Heading>
+            <ChevronLink href="/watches" variant="inverted">
+              Apple Watches catalog
+            </ChevronLink>
+            <ChevronLink href="/iphones" variant="inverted">
+              Iphone catalog
+            </ChevronLink>
+          </Box>
+        </FlexGrid.Col>
+      </FlexGrid.Row>
+    </FlexGrid>
+  </Box>
+);
 
 export default Home;
