562a6ecdc407cdc6c54e75cfbd783c00220caf45
Jose-Vasquez-Telus <104869687+Jose-Vasquez-Telus@users.noreply.github.com>
2022-06-06 09:11:54 -0600

    Create device component

---
+++ src/ui/components/Device/Device.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Heading from "@tds/core-heading";
+import Box from "@tds/core-box";
+import Card from "@tds/core-card";
+import PriceLockup from "@tds/core-price-lockup";
+import HairlineDivider from "@tds/core-hairline-divider";
+import Text from "@tds/core-text";
+import Image from "@tds/core-image";
+import { FootnoteLink } from "@tds/core-terms-and-conditions";
+import FlexGrid from "@tds/core-flex-grid";
+
+const Device = ({ name, brand, price, imgSrc }) => (
+  <div style={{ maxWidth: "24rem", margin: "0 auto" }}>
+    <Box inset={3}>
+      <Card variant="defaultWithBorder">
+        <Box between={4}>
+          <FlexGrid.Row horizontalAlign="center">
+            <Image src={imgSrc} alt="iphone image" width={200} height={200} />
+          </FlexGrid.Row>
+          <Box>
+            <Heading level="h3">{brand}</Heading>
+            <Heading level="h2">{name}</Heading>
+          </Box>
+          <PriceLockup size="medium" price={price} signDirection="left" />
+        </Box>
+        <Box vertical={4}>
+          <HairlineDivider />
+        </Box>
+        <Box between={4}>
+          <Text size="medium">
+            TELUS Easy Payment and Bring-It-Back applied.
+            <FootnoteLink number={[3]} onClick={() => {}} copy="en" />
+            Read legal footnote Taxes due upfront.
+          </Text>
+          <Text size="medium">Retail price: $470</Text>
+        </Box>
+      </Card>
+    </Box>
+  </div>
+);
+export default Device;
