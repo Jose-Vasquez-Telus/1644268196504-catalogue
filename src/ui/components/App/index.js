9e1a356aa29e7253c7489cf2cc20b5faf6f26e1a
Jose-Vasquez-Telus <104869687+Jose-Vasquez-Telus@users.noreply.github.com>
2022-06-06 10:21:30 -0600

    Update index page

--- src/ui/components/App/index.js
+++ src/ui/components/App/index.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Home from '../Home';
-import NotFound from '../NotFound';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "../Home";
+import NotFound from "../NotFound";
+import Watches from "../Watches/Watches";
+import Iphones from "../Iphones/Iphones";
+import "./App.css";
 
 const App = () => (
   <Router>
     <Switch>
+      <Route path="/watches">
+        <Watches />
+      </Route>
+      <Route path="/iphones">
+        <Iphones />
+      </Route>
       <Route path="/">
         <Home />
+      </Route>
+      <Route path="*">
+        <NotFound />
       </Route>
     </Switch>
   </Router>
